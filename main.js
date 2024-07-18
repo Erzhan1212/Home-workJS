f7ff327350da597ee319d0430149cbe51aec5325
Erzhan1212 <moldalieve-1296@mail.ru>
2024-07-19 00:46:33 +0300

    home-3

---
+++ main.js
@@ -0,0 +1,93 @@
+// // ?! Здание№1
+// let num = "10";
+// if (num === 10) {
+//   console.log("верно");
+// } else if (num != 10) {
+//   console.log(" неверно");
+// } else {
+//   console.log("ощибка");
+// }
+
+// ?! Здание№2
+
+// let baatir = "33";
+// let i = prompt("Введите номер квартиры");
+// if (i >= 1 && i <= 20) {
+//   console.log("Подъезд№1");
+// } else if(i>=21 && i<=48){console.log("Подъезд№2");}
+// else if(i>=49 && i<=90){console.log("Подъезд№3");}
+// else{ console.log("Нет такого подъезда");}
+
+// // ?! Здание№3
+// let a = "9";
+
+// if (a > 0 && a < 5) {
+//   console.log("верно");
+// } else if (a != 5) {
+//   console.log(" неверно");
+// } else {
+//   console.log("ощибка");
+// }
+
+// // ?! Здание№4
+
+// const hour = "9";
+// let i = prompt("Какой час?");
+// if (i >= 6 && i <= 12) {
+//   console.log("Доброе утро!");
+// } else if (i >= 13 && i <= 17) {
+//   console.log("Добрый день!");
+// } else {
+//   console.log("Добрый вечер!");
+// }
+
+// // ?! Здание№5
+// const name="Nooruz"
+// let i = prompt("Какой час?");
+// if (i >= 6 && i <= 12) {
+//   console.log("Доброе утро, Nooruz!");
+// } else if (i >= 13 && i <= 17) {
+//   console.log("Добрый день, Nooruz!");
+// } else {
+//   console.log("Добрый вечер, Nooruz!");
+// }
+
+// // ?! Здание№6
+// let skidka="20%"
+// let i = prompt("age");
+// if (i < 6) {
+//   console.log("Скидка");
+// } else if (i >= 65) {
+//   console.log("Скидка");
+// } else {
+//   console.log("Без скидки");
+// }
+
+// ?! Здание№7
+
+//? Зниний и логической обстракции
+
+// ?! Здание№8
+// const number = 2;
+
+// if (number % 2 === 0) {
+//              console.log('Число чётное');
+//  } else {
+//             console.log('Число нечётное');
+//  }
+ `Взятие остатка от деления % -это вычисления операции по модулю,
+ где делитель производит операцию над делимым, 
+ при котором делитель делит делимое без остатков или с 
+ минимальным натуральным числом остатка. Остаток всегда натуральное цело число и меньше делителя.`
+
+ // ?! Здание№9
+ let num = 5;
+
+//  if (num === 5) {
+//       console.log("Five"); 
+// } else {
+//       console.log("Not five"); 
+// }
+
+num===5**true+(true*6-true)
+?console.log("Five"):console.log("Not five");
