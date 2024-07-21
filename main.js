c7d6073e30e9c8a7492420159c4d8f497ccb9e04
Erzhan1212 <moldalieve-1296@mail.ru>
2024-07-21 17:33:38 +0300

    hw-18

---
+++ main.js
@@ -0,0 +1,94 @@
+// //?task#1
+// let num=prompt("num")
+// for(let i=0; i<=num; i++){console.log(i);}
+
+// //?task#2
+// let num=prompt("num")
+// for(let i=0; i<=num; i++)
+//     if( i% 2===0 )
+//     {console.log(i);}
+
+// //?task#3
+// let sum=0;
+// let num=prompt("num")
+// for(let i=0; i<=num; i++)
+//     if( i% 2===0 )
+//       sum=sum+i
+
+//     {console.log("sum=" + sum);}
+// //?task#4
+
+// let searchingChar = prompt("enter any char");
+// let userName = "Naruto Shipuden";
+// let counter = 0;
+// while (counter <= userName.length) {
+// let currentChar = userName.charAt(counter);
+// if (searchingChar === currentChar) {
+//   console.log(`Ооба, мындай символ бар  $(counter) индексте`);
+// }
+//  else {console.log(`Нет такой буква!`);
+
+//  }
+
+//   counter++;
+// }
+// let searchingChar = prompt("enter any char");
+// let userName = "Naruto Shipuden";
+// let counter = 0;
+// while (counter <= userName.length) {
+//     console.log(userName.charAt(counter));
+//   counter++;
+// }
+// let searchingChar = prompt("enter any char");
+// let userName = "Naruto Shipuden";
+// for (let counter=userName.length; counter>=0; counter--){
+//     console.log(userName.charAt(counter));}
+
+//?!task#5
+// let userName = "hello";
+// for (let counter=userName.length; counter>=0; counter--){
+//     console.log(userName.charAt(counter));}
+
+// //?!task#6
+
+// for(let i=0; i<String.length;i++){
+//     console.log(' * '.repeat(10));
+//     console.log(' *'," _ ".repeat(8),"* ");
+//     console.log(' *'," _ ".repeat(8),"* ");
+//     console.log(' *'," _ ".repeat(8),"* ");
+//     console.log(' *'," _ ".repeat(8),"* ");
+//     console.log(' * '.repeat(10));
+// }
+
+//?!task#7
+
+// for(let i=0; i<String.length;i++){
+//     console.log(' # ');
+//     console.log(' #'.repeat(2));
+//     console.log(' #'.repeat(3));
+//     console.log(' #'.repeat(4));
+//     console.log(' #'.repeat(5));
+//     console.log(' #'.repeat(6));
+//     console.log(' #'.repeat(7));
+// }
+
+// p = 0;
+// let outStr = "";
+// let flag = "6";
+
+// while (p < 7) {
+//   let p1 = 0;
+//   while (p1 < 7) {
+//     if (p1 < flag) {
+//       outStr += "";
+//     } else {
+//       outStr += "#";
+//     }
+
+//     p1++;
+//   }
+//   flag--;
+//   outStr += "\n";
+//   p++;
+// }
+// console.log(outStr);
